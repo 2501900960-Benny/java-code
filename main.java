bcba266c74c8872cb75af05cea86a70f1d7467d2
Benard Wambi <wambibenard30@gmail.com>
2026-09-18 10:10:35 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+class Animal{
+    void sound(){
+        System.out.println("Animal makes sound");
+    }
+}
+class Dog extends Animal{
+    @Override
+       void sound(){
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal{
+    @Override
+       void sound(){
+        System.out.println("Cat meows");
+    }
+}
+public class main{
+    public static void main(String[] args){
+        Animal myDog=new Dog();
+        Animal myCat=new Cat();
+        myDog.sound();
+        myCat.sound();
+    }
+}
